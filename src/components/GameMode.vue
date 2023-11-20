da4b982408d80b4ecc4d241715f0b631eb321096
leonidgainar <leonidgainar>
2023-11-20 15:28:51 +0200

    Rename game modes

--- src/components/GameMode.vue
+++ src/components/GameMode.vue
@@ -8,7 +8,7 @@
             class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none align-top mt-1 mr-2 cursor-pointer"
             type="radio" v-model="currentGameMode" value="single" id="single" />
           <label class="form-check-label inline-block text-gray-800" for="single">
-            With Bot
+            Player vs Bot
           </label>
         </div>
         <div class="form-check pt-3 pb-5">
@@ -16,7 +16,7 @@
             class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none align-top mt-1 mr-2 cursor-pointer"
             type="radio" v-model="currentGameMode" value="multiplayer" id="multiplayer" />
           <label class="form-check-label inline-block text-gray-800" for="multiplayer">
-            Two players
+            Player vs Player
           </label>
         </div>
       </div>
